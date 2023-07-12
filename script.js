9029fd7735b7ce90015ae494b09910fe655a51e3
H5Ryder <ryderharris@icloud.com>
2023-07-12 07:46:17 +0000

    Add math operation methods

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function add(a, b) {
-  return a + b;
+//MATH OPERATIONS
+
+const add = (a, b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+
+function operate(a, b, type) {
+  let result = (type === "+") ? add(a,b) :
+  (type === "-") ? subtract(a,b) :
+  (type === "*") ? multiply(a,b) :
+  (type === "/") ? divide(a,b):
+  undefined;
+  
+  return roundTo(result,2);
+;
 }
+
+function roundTo(n, place) {    
+    return +(Math.round(n + "e+" + place) + "e-" + place);
+}
+
+
+
+
+console.log(operate(0.3333,2,"*"));
+
